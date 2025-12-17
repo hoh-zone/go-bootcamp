5688e24c4d1963474b45bfbda447e7b3b8bcd75c
goxiaoy <xy1996mail@gmail.com>
2025-12-17 22:18:44 +0800

    feat: update 03

--- code/03/main.go
+++ code/03/main.go
@@ -1,22 +1,30 @@
 package main
 
 import (
-	"errors"
+	"flag"
 	"fmt"
 	"log"
 	"os"
-	"strings"
 	"time"
 	"unicode/utf8"
 )
+
+var (
+	timeFormat = flag.String("timefmt", time.RFC3339, "layout for printing current time")
+)
+
+func init() {
+	flag.Parse()
+}
 
 func main() {
 	log.SetPrefix("[demo] ")
 	log.SetFlags(0)
 
 	// 1) 打印当前时间与命令行参数
-	fmt.Println("current time:", time.Now().Format(time.RFC3339))
-	fmt.Println("args:", os.Args)
+	fmt.Println("current time:", time.Now().Format(*timeFormat))
+	fmt.Println("args (raw):", os.Args)
+	fmt.Println("args (after flag parsing):", flag.Args())
 
 	// 2) 字符串的字节长度与 rune 数
 	text := "Go语言"
@@ -25,23 +33,4 @@ func main() {
 	for idx, r := range text {
 		fmt.Printf("idx=%d rune=%c\n", idx, r)
 	}
-
-	// 3) 调用返回 (result, error) 的函数并日志输出
-	result, err := greetFromArgs(os.Args)
-	if err != nil {
-		log.Printf("greet error: %v", err)
-		return
-	}
-	log.Printf("greet result: %s", result)
-}
-
-func greetFromArgs(args []string) (string, error) {
-	if len(args) < 2 {
-		return "", errors.New("need at least one name argument")
-	}
-	name := strings.TrimSpace(args[1])
-	if name == "" {
-		return "", errors.New("name cannot be empty")
-	}
-	return fmt.Sprintf("hello, %s", strings.ToUpper(name)), nil
 }
